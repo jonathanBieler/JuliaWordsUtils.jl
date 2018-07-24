620d8d9449b425df6bf80f602856c9fd21ed5b33
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2018-07-24 16:34:34 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # JuliaWordsUtils
 
+A couple of utility functions to select Julia words from in a String.
+
+Examples:
+
+```julia
+  get_word_backward(4,"@static") == "@sta"
+  select_word_backward(3,"@static") == (1,3)
+  extend_word(3, "A.B.C") == ("B", 3, 3)
+  extend_word(3, "A.B.C",false) == ("A.B.C", 1, 5)
+```
+
 [![Build Status](https://travis-ci.org/jonathanBieler/JuliaWordsUtils.jl.svg?branch=master)](https://travis-ci.org/jonathanBieler/JuliaWordsUtils.jl)
 
 [![Coverage Status](https://coveralls.io/repos/jonathanBieler/JuliaWordsUtils.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/jonathanBieler/JuliaWordsUtils.jl?branch=master)
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,43 @@ else
     using Test
 end
 
+tostring(s,ij) = s[ij[1]:ij[2]]
+
 # write your own tests here
-@test 1 == 2
+@testset "select_word_backward" begin
+    @test select_word_backward(3,"1234") == (1,3)
+    @test select_word_backward(3,"1234",true) == (1,3)
+
+    @test select_word_backward(10,"select_word_backward") == (1,10)
+    @test select_word_backward(4,"1(34") == (3,4)
+
+    @test select_word_backward(3,"A.B",true) == (3,3)
+    @test select_word_backward(3,"A.B",false) == (1,3)
+
+    @test select_word_backward(3,"@static") == (1,3)
+end
+
+@testset "get_word_backward" begin
+    @test get_word_backward(4,"@static") == "@sta"
+
+    @test get_word_backward(9,"CharArray(s,length(s))") == "CharArray"
+    @test get_word_backward(10,"CharArray(s,length(s))") == "CharArray("
+    @test get_word_backward(11,"CharArray(s,length(s))") == "s"
+    @test get_word_backward(18,"CharArray(s,length(s))") == "length"
+
+    @test get_word_backward(4,"]αβγ") == "αβγ"
+
+    @test get_word_backward(5,"push!") == "push!"
+    @test get_word_backward(5,"!push") == "push"
+
+    @test get_word_backward(5,"push[") == "["
+end
+
+@testset "extend_word" begin
+
+    @test extend_word(1, "CharArray(s,length(s))") == ("CharArray", 1, 9)
+    @test extend_word(1, "A.B.C") == ("A", 1, 1)
+    @test extend_word(2, "A.B.C") == (".", 2, 2)
+    @test extend_word(3, "A.B.C") == ("B", 3, 3)
+    @test extend_word(3, "A.B.C",false) == ("A.B.C", 1, 5)
+end
